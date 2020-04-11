8c4263871b2ce1768ff33a76d45084f27897c4f5
Evaldo Santos <jesantosjr@protonmail.com>
2020-04-11 18:40:48 +0200

    handling functional components

--- packages/create-reacty-app/index.js
+++ packages/create-reacty-app/index.js
@@ -1,10 +1,14 @@
 import reacty from '../reacty';
 
+// functional component to welcome someone
+const Greeting = ({ name }) => <p>Welcome {name}!</p>;
+
 const App = (
   <div>
     <h1 className="primary">
       reacty is quick and dirty react
     </h1>
+    <Greeting name="Guest" />
   </div>
 );
 
--- packages/reacty/createElement.js
+++ packages/reacty/createElement.js
@@ -1,6 +1,11 @@
 import { h } from 'snabbdom';
 
 const createElement = (type, props = {}, ...children) => {
+  // if type is a function then call it and return it's value
+  if (typeof (type) == 'function') {
+    return type(props);
+  }
+  
   return h(type, { props }, children);
 };
 
